47099e488951258cc269d146305a81ba65187ad5
Erik <erik851c@stud.kea.dk>
2020-05-18 15:01:18 +0200

    Maybe this will work?

---
+++ src/main/java/dat19d/group/six/motorhomerental/MotorHomeRentalApplication.java
@@ -0,0 +1,13 @@
+package dat19d.group.six.motorhomerental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorHomeRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotorHomeRentalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dat19d/group/six/motorhomerental/MotorHomeRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package dat19d.group.six.motorhomerental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorHomeRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
